58e176c0ad6f94aef482bdbc1003f535040f143f
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-03-12 18:10:17 +0300

    Implemented Repository pattern

---
+++ Application/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Domain.Contracts;
+
+namespace Application.Repository;
+
+public interface IRepository<T> where T : IEntity
+{
+    IQueryable<T> Entities { get; }
+    Task<IQueryable<T>> GetByConditionAsync(Expression<Func<T, bool>> expression);
+    Task<ICollection<T>> GetAllAsync();
+    Task<T> CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
