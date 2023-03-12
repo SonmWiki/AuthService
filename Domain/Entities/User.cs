c309d1161646c0827ac0976c111da88bb4e3440c
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-03-12 19:09:38 +0300

    Added IsVerified field to User

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -7,4 +7,5 @@ public class User : Entity<Guid>
     public string Email { get; set; }
     public string UserName { get; set; }
     public string PasswordHash { get; set; }
+    public bool IsVerified { get; set; }
 }
