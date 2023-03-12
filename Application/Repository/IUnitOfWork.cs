30f5f57f332b1b2922dc98a091fbfadb0d52be19
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-03-12 18:37:28 +0300

    Implemented Unit of Work pattern

---
+++ Application/Repository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Repository;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUserRepository UserRepository { get; }
+    IUserRoleRepository UserRoleRepository { get; }
+    IRolePermissionRepository RolePermissionRepository { get; }
+    
+    Task<int> CommitAsync(CancellationToken cancellationToken);
+
+    Task RollbackAsync();
+}
